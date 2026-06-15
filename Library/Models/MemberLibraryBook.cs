457b620a7f57875c4258733eb2368544924cd201
VeerShahCodes <shahveer1213@gmail.com>
2026-06-14 20:15:55 -0700

    made more sql command and stuff almost done. almost can move on to api and frontend

---
+++ Library/Models/MemberLibraryBook.cs
@@ -0,0 +1,16 @@
+﻿namespace Library.Models
+{
+    public class MemberLibraryBook
+    {
+        public int member_id { get; set; }
+        public int library_id { get; set; }
+        public int book_id { get; set; }
+
+        public MemberLibraryBook(int member_id, int library_id, int book_id)
+        {
+            this.member_id = member_id;
+            this.book_id = book_id;
+            this.library_id = library_id;
+        }
+    }
+}
